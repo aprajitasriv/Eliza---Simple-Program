8438a4e1d83606481eb322bf94df06869063a230
aprajitasriv <48492286+aprajitasriv@users.noreply.github.com>
2019-04-10 16:43:11 -0400

    Update 20190410 UPDATED Eliza - Simple-Final.py

--- 20190410 UPDATED Eliza - Simple-Final.py
+++ 20190410 UPDATED Eliza - Simple-Final.py
@@ -1,10 +1,5 @@
 
-question = True
-while question:
-    str = input('Good day. What is your problem? Enter your response here or Q to quit:')
-    print(str)
-    if str.lower() == "great" or str.lower() == "q":
-        question = False
-
-print('End')
-
+str = input('Good day. What is your problem? Enter your response here or Q to quit:')
+print(str)
+if str.lower() == "great" or str.lower() == "q":
+    question = False
